a7f5c2bc8a08e6b92cd9be6fa162bb48cb0117e0
PavankumarAN <pavankumaran51@gmail.com>
2019-01-14 18:41:09 +0530

    Weather App

---
+++ AsyncWeatherApp/Basics/async-basics.js
@@ -0,0 +1,12 @@
+console.log(`Starting the app`);
+
+setTimeout(() =>{
+console.log(`Time out`);
+},2000);
+
+setTimeout(() =>{
+    console.log(`Zero seconds`);
+    
+},0);
+
+console.log(`Stopping the app`);
